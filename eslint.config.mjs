cfd17e04b0ec109f139eeca8ea70b4bc978681d6
beefchimi <curtdulmage@gmail.com>
2024-07-07 20:55:57 -0400

    :art: [Prettier] Format

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,4 @@
-import eslintPluginAstro from "eslint-plugin-astro";
+import eslintPluginAstro from 'eslint-plugin-astro';
 
 export default [
   // { ignores: ['coverage/**', 'dist/**'], },
@@ -6,11 +6,11 @@ export default [
   // Add more generic rule sets here, such as:
   // js.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
-  ...eslintPluginAstro.configs["jsx-a11y-recommended"],
+  ...eslintPluginAstro.configs['jsx-a11y-recommended'],
 
   {
-    name: "custom-rules",
-    files: ["**/*.astro", "**/*.ts", "**/*.js", "**/*.mjs"],
+    name: 'custom-rules',
+    files: ['**/*.astro', '**/*.ts', '**/*.js', '**/*.mjs'],
     rules: {
       // Override / add rules settings here, such as:
       // "astro/no-set-html-directive": "error"
