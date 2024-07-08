c6bcc8ce3c13a40d7c14af8340a6bedf43255713
beefchimi <curtdulmage@gmail.com>
2024-07-07 20:49:59 -0400

    :tada: [Repo] Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import eslintPluginAstro from "eslint-plugin-astro";
+
+export default [
+  // { ignores: ['coverage/**', 'dist/**'], },
+
+  // Add more generic rule sets here, such as:
+  // js.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+  ...eslintPluginAstro.configs["jsx-a11y-recommended"],
+
+  {
+    name: "custom-rules",
+    files: ["**/*.astro", "**/*.ts", "**/*.js", "**/*.mjs"],
+    rules: {
+      // Override / add rules settings here, such as:
+      // "astro/no-set-html-directive": "error"
+    },
+  },
+];
