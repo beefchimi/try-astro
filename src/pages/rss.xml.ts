e1f8ed235e5c7beb45f5ecc4060cd9b7de66e23d
beefchimi <curtdulmage@gmail.com>
2024-07-09 10:56:26 -0400

    :bug: [Types] Fix some type issues

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,10 +1,11 @@
 import rss, {pagesGlobToRssItems} from '@astrojs/rss';
 
-export async function GET(context) {
+// TODO: Figure out how to type `context` arg.
+export async function GET({site = ''}) {
   return rss({
     title: 'Astro Learner | Blog',
     description: 'My journey learning Astro',
-    site: context.site,
+    site,
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
     customData: `<language>en-us</language>`,
   });
